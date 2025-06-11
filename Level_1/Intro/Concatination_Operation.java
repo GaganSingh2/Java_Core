c498c94f25823b239ce326c2d1d21d6cbb65d44b
GaganSingh2 <gks022004@gmail.com>
2025-06-11 22:47:49 +0530

    "Concatination_Operation using + Operator"

---
+++ Level_1/Intro/Concatination_Operation.java
@@ -0,0 +1,7 @@
+public class Concatination_Operation {
+    public static void main(String[] args) {
+        System.out.println("Hello " + "World");
+        System.out.println("Hello " + 10);
+        System.out.println(10 + " World");
+    }
+}
