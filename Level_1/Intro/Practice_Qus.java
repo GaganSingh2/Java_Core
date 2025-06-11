82eaf4b6dd27e3a5fb995a913c4c179a17c765de
GaganSingh2 <gks022004@gmail.com>
2025-06-11 22:59:02 +0530

    "Practice Qus"

---
+++ Level_1/Intro/Practice_Qus.java
@@ -0,0 +1,16 @@
+/*  Q)WAP to print the following statement:
+        1)Full Name
+        2)YOP
+        3)CGPA
+        4)GEnder
+        5)Hobbies
+*/
+public class Practice_Qus {
+    public static void main(String[] args) {
+        System.out.println("My Name is: Gagan Kumar Singh");
+        System.out.println("YOP: "+2026);
+        System.out.println("CGPA: "+8.40);
+        System.out.println("Gender: M");
+        System.out.println("Hobbies: Playing Cricket, Traveling");
+    }
+}
