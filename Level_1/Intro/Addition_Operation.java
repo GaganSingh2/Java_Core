f1b96f917d7dd8ee14dd23c487b100b91053e43b
GaganSingh2 <gks022004@gmail.com>
2025-06-11 22:43:55 +0530

    "Addition_Operation"

---
+++ Level_1/Intro/Addition_Operation.java
@@ -0,0 +1,7 @@
+public class Addition_Operation {
+    public static void main(String[] args) {
+        System.out.println(10+30);
+        System.out.println(1+3.0);
+        System.out.println(1.0+3);
+    }
+}
---
+++ Level_1/Intro/First.java
@@ -0,0 +1,7 @@
+// package Intro;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Radhey Radhey!");
+    }
+}
