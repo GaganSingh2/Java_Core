a9a20b33c05c9b3b0cea947f99f9f479ac7a17ed
GaganSingh2 <gks022004@gmail.com>
2025-06-11 23:00:29 +0530

    "ASCII value"

--- Level_1/Intro/Concatination_Operation.java
+++ Level_1/Intro/Concatination_Operation.java
@@ -3,5 +3,6 @@ public class Concatination_Operation {
         System.out.println("Hello " + "World");
         System.out.println("Hello " + 10);
         System.out.println(10 + " World");
+        System.out.println('A'+10); // ASCII value of 'A' is 65, so it will print 75
     }
 }
