ae087f0fdaf824bd41e322ee7735a6fc40e13d82
GaganSingh2 <gks022004@gmail.com>
2025-06-12 11:57:59 +0530

    "My Intro"

---
+++ Level_1/Intro/My_Intro.java
@@ -0,0 +1,7 @@
+class My_Intro{
+    public static void main(String[] args) {
+        System.out.println("My Name is: Gagan Kumar Singh");
+        System.out.println("CGPA: "+8.40);
+        System.out.println("Hobbies: Playing Cricket, Travelling");
+    }
+}
