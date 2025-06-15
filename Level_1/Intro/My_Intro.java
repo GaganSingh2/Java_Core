820ec47b48374cbca5f77a21685a0e183844c673
GaganSingh2 <gks022004@gmail.com>
2025-06-15 23:31:38 +0530

    "points"

--- Level_1/Intro/My_Intro.java
+++ Level_1/Intro/My_Intro.java
@@ -3,5 +3,9 @@ class My_Intro{
         System.out.println("My Name is: Gagan Kumar Singh");
         System.out.println("CGPA: "+8.40);
         System.out.println("Hobbies: Playing Cricket, Travelling");
+
+        int val = 40;
+        float fval = 40.34f;
+        System.out.println("Value: "+val+" Float: "+fval);
     }
 }
